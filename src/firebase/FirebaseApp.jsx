99493120361174a7de53fe60706b9d9d282e9392
Mariana Martinez Araque <marianadelos.martinez@unet.edu.ve>
2023-08-02 16:01:11 -0400

    firebase connection

---
+++ src/firebase/FirebaseApp.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable prettier/prettier */
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyB-O3kB8Y4sREuc28oFr061XVI4V3QvVD8',
+  authDomain: 'automatizacion-web-app-2023-1.firebaseapp.com',
+  databaseURL: 'https://automatizacion-web-app-2023-1-default-rtdb.firebaseio.com',
+  projectId: 'automatizacion-web-app-2023-1',
+  storageBucket: 'automatizacion-web-app-2023-1.appspot.com',
+  messagingSenderId: '957223053454',
+  appId: '1:957223053454:web:45cf99b036c782ce81c1df',
+  measurementId: 'G-722GLHK95N',
+}
+
+// Initialize Firebase
+export const FirebaseApp = initializeApp(firebaseConfig)
+const analytics = getAnalytics(FirebaseApp)
--- src/views/pages/login/Login.js
+++ src/views/pages/login/Login.js
@@ -63,10 +63,7 @@ const Login = () => {
                 <CCardBody className="text-center">
                   <div>
                     <h2>Sign up</h2>
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                      tempor incididunt ut labore et dolore magna aliqua.
-                    </p>
+                    <p>Automatizacion web app con firebase database realtime</p>
                     <Link to="/register">
                       <CButton color="primary" className="mt-3" active tabIndex={-1}>
                         Register Now!
