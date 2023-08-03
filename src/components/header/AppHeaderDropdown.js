f5a54566f6fed05cbcc31a74c65efcdecf9550b6
Mariana Martinez Araque <marianadelos.martinez@unet.edu.ve>
2023-08-03 16:28:14 -0400

    ya trae la informacion de los tipos de procesadores en la tabla

--- src/components/header/AppHeaderDropdown.js
+++ src/components/header/AppHeaderDropdown.js
@@ -1,14 +1,5 @@
 import React from 'react'
-import {
-  CAvatar,
-  CDropdown,
-  CDropdownHeader,
-  CDropdownItem,
-  CDropdownMenu,
-  CDropdownToggle,
-} from '@coreui/react'
-import { cilUser, cilAccountLogout } from '@coreui/icons'
-import CIcon from '@coreui/icons-react'
+import { CAvatar, CDropdown, CDropdownToggle } from '@coreui/react'
 
 import avatar8 from './../../assets/images/avatars/8.jpg'
 
@@ -18,17 +9,6 @@ const AppHeaderDropdown = () => {
       <CDropdownToggle placement="bottom-end" className="py-0" caret={false}>
         <CAvatar src={avatar8} size="md" />
       </CDropdownToggle>
-      <CDropdownMenu className="pt-0" placement="bottom-end">
-        <CDropdownHeader className="bg-light fw-semibold py-2">Account</CDropdownHeader>
-        <CDropdownItem href="#">
-          <CIcon icon={cilUser} className="me-2" />
-          Profile
-        </CDropdownItem>
-        <CDropdownItem href="#">
-          <CIcon icon={cilAccountLogout} className="me-2" />
-          Log Out
-        </CDropdownItem>
-      </CDropdownMenu>
     </CDropdown>
   )
 }
--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 
 import {
-  CAvatar,
   CCard,
   CCardBody,
   CCardHeader,
@@ -18,28 +17,8 @@ import {
   CTableRow,
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
-import {
-  cibCcAmex,
-  cibCcApplePay,
-  cibCcMastercard,
-  cibCcPaypal,
-  cibCcStripe,
-  cibCcVisa,
-  cifBr,
-  cifEs,
-  cifFr,
-  cifIn,
-  cifPl,
-  cifUs,
-  cilPeople,
-} from '@coreui/icons'
+import { cilCheckCircle, cilXCircle } from '@coreui/icons'
 
-import avatar1 from 'src/assets/images/avatars/1.jpg'
-import avatar2 from 'src/assets/images/avatars/2.jpg'
-import avatar3 from 'src/assets/images/avatars/3.jpg'
-import avatar4 from 'src/assets/images/avatars/4.jpg'
-import avatar5 from 'src/assets/images/avatars/5.jpg'
-import avatar6 from 'src/assets/images/avatars/6.jpg'
 import ModalDatabase from 'src/components/ModalDatabase'
 import { FirebaseApp, firebaseConfig } from 'src/firebase/FirebaseApp'
 import { getDatabase, onValue, ref } from 'firebase/database'
@@ -58,7 +37,16 @@ const Dashboard = () => {
     const procesadores_info = ref(database, 'procesadores/')
     onValue(procesadores_info, (snapshot) => {
       const data = snapshot.val()
-      setProcesadores(data)
+
+      let list_keys = Object.keys(data)
+
+      let list_objetos_procesadores = []
+
+      for (var i = 0; i < list_keys.length; i++) {
+        list_objetos_procesadores.push(data[list_keys[i]])
+      }
+
+      setProcesadores(list_objetos_procesadores)
     })
   }
 
@@ -72,7 +60,6 @@ const Dashboard = () => {
       let total_vector = 0
 
       for (var i = 0; i < registros.length; i++) {
-        console.log(registros[i])
         let vector = registros[i]
         total_vector += vector[1]
       }
@@ -97,102 +84,8 @@ const Dashboard = () => {
     return (cantidad * 100) / total
   }
 
-  console.log(procesadores)
-
-  const tableExample = [
-    {
-      avatar: { src: avatar1, status: 'success' },
-      user: {
-        name: 'Yiorgos Avraamu',
-        new: true,
-        registered: 'Jan 1, 2021',
-      },
-      country: { name: 'USA', flag: cifUs },
-      usage: {
-        value: 50,
-        period: 'Jun 11, 2021 - Jul 10, 2021',
-        color: 'success',
-      },
-      payment: { name: 'Mastercard', icon: cibCcMastercard },
-      activity: '10 sec ago',
-    },
-    {
-      avatar: { src: avatar2, status: 'danger' },
-      user: {
-        name: 'Avram Tarasios',
-        new: false,
-        registered: 'Jan 1, 2021',
-      },
-      country: { name: 'Brazil', flag: cifBr },
-      usage: {
-        value: 22,
-        period: 'Jun 11, 2021 - Jul 10, 2021',
-        color: 'info',
-      },
-      payment: { name: 'Visa', icon: cibCcVisa },
-      activity: '5 minutes ago',
-    },
-    {
-      avatar: { src: avatar3, status: 'warning' },
-      user: { name: 'Quintin Ed', new: true, registered: 'Jan 1, 2021' },
-      country: { name: 'India', flag: cifIn },
-      usage: {
-        value: 74,
-        period: 'Jun 11, 2021 - Jul 10, 2021',
-        color: 'warning',
-      },
-      payment: { name: 'Stripe', icon: cibCcStripe },
-      activity: '1 hour ago',
-    },
-    {
-      avatar: { src: avatar4, status: 'secondary' },
-      user: { name: 'Enéas Kwadwo', new: true, registered: 'Jan 1, 2021' },
-      country: { name: 'France', flag: cifFr },
-      usage: {
-        value: 98,
-        period: 'Jun 11, 2021 - Jul 10, 2021',
-        color: 'danger',
-      },
-      payment: { name: 'PayPal', icon: cibCcPaypal },
-      activity: 'Last month',
-    },
-    {
-      avatar: { src: avatar5, status: 'success' },
-      user: {
-        name: 'Agapetus Tadeáš',
-        new: true,
-        registered: 'Jan 1, 2021',
-      },
-      country: { name: 'Spain', flag: cifEs },
-      usage: {
-        value: 22,
-        period: 'Jun 11, 2021 - Jul 10, 2021',
-        color: 'primary',
-      },
-      payment: { name: 'Google Wallet', icon: cibCcApplePay },
-      activity: 'Last week',
-    },
-    {
-      avatar: { src: avatar6, status: 'danger' },
-      user: {
-        name: 'Friderik Dávid',
-        new: true,
-        registered: 'Jan 1, 2021',
-      },
-      country: { name: 'Poland', flag: cifPl },
-      usage: {
-        value: 43,
-        period: 'Jun 11, 2021 - Jul 10, 2021',
-        color: 'success',
-      },
-      payment: { name: 'Amex', icon: cibCcAmex },
-      activity: 'Last week',
-    },
-  ]
-
   return (
     <>
-      {console.log(ModalVisible)}
       {ModalVisible ? <ModalDatabase></ModalDatabase> : setModalVisible(false)}
       {Loading ? (
         <CSpinner color="primary"></CSpinner>
@@ -204,7 +97,7 @@ const Dashboard = () => {
           <CRow>
             <CCol xs>
               <CCard className="mb-4">
-                <CCardHeader>Database Firebase</CCardHeader>
+                <CCardHeader>Reporte cantidad de computadoras vendidas por años</CCardHeader>
                 <CCardBody>
                   <CRow>
                     <CCol>
@@ -237,11 +130,10 @@ const Dashboard = () => {
                           <div className="progress-group-bars">
                             <CProgress height={20} variant="striped" animated>
                               <CProgressBar
-                                color="success"
                                 value={calcular_porcentaje(vector[1], total_vendidos)}
-                                className="overflow-visible text-dark px-2"
+                                className="overflow-visible text-center text-light bg-dark px-3 font-weight-bold"
                               >
-                                {calcular_porcentaje(vector[1], total_vendidos)}
+                                {calcular_porcentaje(vector[1], total_vendidos).toFixed(1)}
                               </CProgressBar>
                             </CProgress>
                           </div>
@@ -249,63 +141,67 @@ const Dashboard = () => {
                       ))}
                     </CCol>
                   </CRow>
-
-                  <br />
-
+                </CCardBody>
+              </CCard>
+            </CCol>
+          </CRow>
+          <CRow>
+            <CCard className="mb-4">
+              <CCardHeader>Datos ventas por Modelos de procesadores</CCardHeader>
+              <CCardBody>
+                <h2 className="text-center p-3">Datos ventas por Modelos de procesadores</h2>
+                <br></br>
+                <CRow>
                   <CTable align="middle" className="mb-0 border" hover responsive>
                     <CTableHead color="light">
                       <CTableRow>
-                        <CTableHeaderCell className="text-center">
-                          <CIcon icon={cilPeople} />
-                        </CTableHeaderCell>
-                        <CTableHeaderCell>User</CTableHeaderCell>
-                        <CTableHeaderCell className="text-center">Country</CTableHeaderCell>
-                        <CTableHeaderCell>Usage</CTableHeaderCell>
-                        <CTableHeaderCell className="text-center">Payment Method</CTableHeaderCell>
-                        <CTableHeaderCell>Activity</CTableHeaderCell>
+                        <CTableHeaderCell className="text-center">nombre</CTableHeaderCell>
+                        <CTableHeaderCell className="text-center">nucleos</CTableHeaderCell>
+                        <CTableHeaderCell className="text-center">velocidad</CTableHeaderCell>
+                        <CTableHeaderCell className="text-center">porcentaje</CTableHeaderCell>
+                        <CTableHeaderCell className="text-center">64bits</CTableHeaderCell>
+                        <CTableHeaderCell className="text-center">cantidad</CTableHeaderCell>
                       </CTableRow>
                     </CTableHead>
                     <CTableBody>
-                      {tableExample.map((item, index) => (
+                      {procesadores.map((item, index) => (
                         <CTableRow v-for="item in tableItems" key={index}>
                           <CTableDataCell className="text-center">
-                            <CAvatar size="md" src={item.avatar.src} status={item.avatar.status} />
-                          </CTableDataCell>
-                          <CTableDataCell>
-                            <div>{item.user.name}</div>
-                            <div className="small text-medium-emphasis">
-                              <span>{item.user.new ? 'New' : 'Recurring'}</span> | Registered:{' '}
-                              {item.user.registered}
-                            </div>
+                            <div>{item.nombre}</div>
                           </CTableDataCell>
                           <CTableDataCell className="text-center">
-                            <CIcon size="xl" icon={item.country.flag} title={item.country.name} />
+                            <div>{item.nucleos}</div>
+                          </CTableDataCell>
+                          <CTableDataCell className="text-center">
+                            <div>{item.velocidad}</div>
                           </CTableDataCell>
                           <CTableDataCell>
                             <div className="clearfix">
                               <div className="float-start">
-                                <strong>{item.usage.value}%</strong>
-                              </div>
-                              <div className="float-end">
-                                <small className="text-medium-emphasis">{item.usage.period}</small>
+                                <strong>{item.porcentaje}%</strong>
                               </div>
                             </div>
-                            <CProgress thin color={item.usage.color} value={item.usage.value} />
+                            <CProgress thin color="info" value={item.porcentaje} />
                           </CTableDataCell>
                           <CTableDataCell className="text-center">
-                            <CIcon size="xl" icon={item.payment.icon} />
+                            {item.acepta64bits ? (
+                              <CIcon icon={cilCheckCircle} className="text-success" size="xl" />
+                            ) : (
+                              <CIcon icon={cilXCircle} className="text-danger" size="xl" />
+                            )}
                           </CTableDataCell>
-                          <CTableDataCell>
-                            <div className="small text-medium-emphasis">Last login</div>
-                            <strong>{item.activity}</strong>
+                          <CTableDataCell className="text-center">
+                            <div className="small text-font-bold">
+                              <strong>{item.cantidad}</strong>
+                            </div>
                           </CTableDataCell>
                         </CTableRow>
                       ))}
                     </CTableBody>
                   </CTable>
-                </CCardBody>
-              </CCard>
-            </CCol>
+                </CRow>
+              </CCardBody>
+            </CCard>
           </CRow>
         </>
       )}
